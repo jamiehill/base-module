fd9ead6d8eff5cffbd90bffc4a9ff4481bd6f2a4
jamie <jamie@tiltdigital.com>
2015-06-27 07:53:49 +0100

    module self contained - runnable from module.html

--- src/js/module.js
+++ src/js/module.js
@@ -1,54 +1,97 @@
-import AppLayout from './AppLayout';
-import DeferredQueue from 'core/defer/DeferredQueue';
-import Backbone from 'backbone';
+import Radio from 'backbone.radio';
+import timestamp from 'core/system/NiceConsole';
+
+import React from 'react';
+import View from 'core/system/react/ReactView';
+import Component from './highlights/HighlightsView.jsx!';
+import config from './configuration.json!';
+import globals from 'core/Globals';
 
 var Application = Marionette.Application.extend({
-    bootstrap: [
-        'app/AppConfig',
-        'core/bootstrap/DomainResolver'
-    ],
+
+	// setup our channels
+	session: Radio.channel('session'),
+	socket:  Radio.channel('socket'),
+	router:  Radio.channel('router'),
+	bus:  	 Radio.channel('bus'),
+
+
+	bootstrap: [
+		//'highlights/HighlightsConfig',
+		'core/system/bootstrap/DomainResolver',
+		'core/system/bootstrap/MarionetteConfig',
+		'core/system/bootstrap/GetSportData'
+	],
 
 
     initialize() {
-        this.$body  = $(document.body);
-        this.ctx = di.createContext();
+		_.bindAll(this, 'start');
 
-        // initialize region/s
-        this.addRegions({"main": "body"});
-        this.prestart();
+		window.App = this;
+		window.ctx = this.ctx = di.createContext();
+
+		// initialize src layout
+		this.addRegions({"main": "body"});
+
+		timestamp(console);
+		this.prestart();
     },
 
 
-    prestart() {
-        console.log('Module: Bootstrap - Start');
-        var queue = new DeferredQueue(this.bootstrap),
-            that  = this;
-        queue.init().then(function() {
-            console.log('Module: Bootstrap - Complete');
-            that.start();
-        });
-    },
+	/**
+	 * On start kick off the views
+	 */
+	onStart() {
+		console.log('App: Start');
+		this.ctx.initialize();
+
+		var module = App.module('Module', Module);
+		module.regionName = 'main';
+		module.viewClass = View;
+		module.options = {
+			component: React.createFactory(Component)
+		};
+		module.start();
+
+		this.postStart();
+	},
+
+	/**
+	 * Shut down applicatiopn
+	 */
+	onStop() {
+		console.log("Backbone: history - stopped");
+		Backbone.history.stop();
+		Radio.reset();
+	},
 
 
-    onStart() {
-        console.log('App: Start');
-        this.ctx.initialize();
-        var module;
+	/**
+	 * kick the boot sequence off
+	 */
+	prestart() {
+		console.log('App: PreStart');
 
-        System.import(pkg.main).then(function(view) {
-            module = App.module('Module', Module);
-            module.regionName = 'main'
-            module.view = view;
-            module.start();
+		App.Config  = config;
+		App.Globals = new globals;
+		var that = this;
 
-            // then startup the routers
-            Backbone.history.start({pushState: true, root: this.Urls.root || ''});
-        })
-    },
+		System.import('core/CoreModule').then(function(inst){
+			var module = App.module('Core', inst.default);
+			module.boot(that.bootstrap).then(that.start);
+		});
+	},
 
-    stop() {
-        Backbone.history.stop();
-    }
+	/**
+	 * Startup router and history
+	 */
+	postStart() {
+		console.log('App: PostStart');
+		//this.Router = appRouter.start();
+
+		var options = {pushState: true, root: this.Urls.root || ''};
+		Backbone.history.start(options);
+	}
 });
 
 
@@ -57,23 +100,21 @@ var Application = Marionette.Application.extend({
  * @type {void|Object}
  */
 var Module = Marionette.Module.extend({
-    startWithParent: false,
-    regionName: '',
-    view: null,
+	startWithParent: false,
+	regionName: '',
+	viewClass: null,
+	onStart() {
+		console.log("Module: started");
+		this.view = new this.viewClass(this.options);
+		this.region = this.app[this.regionName];
 
-    onStart() {
-        console.log("Module: started");
-
-        // attach the view
-        if (this.view && this.regionName) {
-            this.app[this.regionName].show(new this.view());
-        }
-    },
+		if (this.viewClass && this.regionName) {
+			this.region.show(this.view);
+		}
+	},
 });
 
 
 // exports singleton instance
 let inst = new Application();
-window.App = inst;
-window.ctx = inst.ctx;
 export default inst;
